a7c34f62c3cae795b940254ec7350d1a187d8099
Leon Bremer <leon_bremer@hotmail.com>
2022-11-02 10:33:16 +0100

    First file uploads.

---
+++ README.md
@@ -0,0 +1,5 @@
+# README - Thesis support sessions for STREEM students: Planning
+
+This repository holds the slides and some small R code produced for a 45 minutes session on Planning for STREEM Master students. If interested in the source code for the slides, please contact me.
+
+The R script holds the code to produce a simple GANTT chart. Feel free to find other planning tools online. But in case you are interested in learning some R in the process, go ahead and make your own planning using the script. The script reads in an excel file with data. The produced example GANTT can also be found in this repository.
---
+++ code/gantt.R
@@ -0,0 +1,51 @@
+########################################
+# Purpose:  Simple GANTT chart for research planning. Example for STREEM
+#           students.
+# Date:     Nov 1, 2022
+# Author:   Leon Bremer
+########################################
+
+### Load external packages
+library(tidyverse)
+library(readxl)
+
+### Setup
+setwd('C:/Users/abr448/Documents/VU/TSS/TSS2022-23')
+
+### Data
+tbl <- read_excel("data/gantt_dates.xlsx") %>%
+  mutate(
+    Stage = factor(x = Stage,
+                   levels = 1:6,
+                   labels = c('Orientation', 'ResQuestion', 'LitRev',
+                              'Method & Data', 'Analysis', 'Writing')),
+    Start = as.Date(Start),
+    End = as.Date(End)
+  )
+
+### Plot
+ggplot(data = tbl,
+       mapping = aes(x = Start, xend = End, y = Stage, yend = Stage,
+                     color = Importance)) +
+  annotate('rect', xmin = as.Date('2023-01-01'), xmax = as.Date('2023-01-31'),
+           ymin = .5, ymax = tbl$Stage %>% levels() %>% length() + .5,
+           fill = '#6fb3e3', alpha = .35) +
+  annotate('rect', xmin = as.Date('2023-03-21'), xmax = as.Date('2023-06-30'),
+           ymin = .5, ymax = tbl$Stage %>% levels() %>% length() + .5,
+           fill = '#6fb3e3', alpha = .35) +
+  geom_segment(size = 7) +
+  labs(title = 'GANTT chart',
+       subtitle = 'Planning for STREEM thesis',
+       caption = 'EXAMPLE by Leon Bremer') +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %y") +
+  scale_y_discrete(limits = rev) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    axis.title = element_blank()
+  ) +
+  scale_color_continuous(low = '#d9db4b', high = '#e65b05')
+
+ggsave('figures/gantt_example.pdf',
+       width = 8,
+       height = 3)
